79770e5a0bc74d64e33e1863f124664c145fb544
shaik iliyas <allah@shaiks-MacBook-Air.local>
2026-07-13 00:40:33 +0530

    Reapply "Refactor money entry into full-height bottom sheet on save branch"
    This reverts commit c9c9104557761c73b895e167228ff1a829290e7c.

--- features/more/app-version.js
+++ features/more/app-version.js
@@ -5,8 +5,8 @@
 (function (global) {
     'use strict';
 
-    const APP_VERSION = "1.0.78";
-    const APP_BUILT_AT = "12 Jul 2026 · 11:40 pm";
+    const APP_VERSION = "1.0.79";
+    const APP_BUILT_AT = "13 Jul 2026 · 12:30 am";
 
     function paintMoreHubBuildMeta() {
         const el = document.getElementById('more-hub-build-meta');
